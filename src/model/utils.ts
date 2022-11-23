2de08a982129490ad6fe54cf707f0652673902d2
Karsten Schmidt <k@thi.ng>
2022-11-24 00:27:17 +0100

    feat: update "breaking change" check

--- src/model/utils.ts
+++ src/model/utils.ts
@@ -2,5 +2,4 @@ import type { Commit } from "./api.js";
 
 export const isPublish = (x: Commit) => x.title.toLowerCase() === "publish";
 
-export const isBreakingChangeMsg = (x: string) =>
-	x.startsWith("BREAKING CHANGE:");
+export const isBreakingChangeMsg = (x: string) => /^BREAKING CHANGES?:/.test(x);
